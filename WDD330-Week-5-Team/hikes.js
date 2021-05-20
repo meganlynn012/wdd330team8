5f8e79d28719e9a1fee565888de99660d6a294bd
Montana Burr <bur18019@byui.edu>
2021-05-20 15:17:54 -0600

    Added Week 5 code.

---
+++ WDD330-Week-5-Team/hikes.js
@@ -0,0 +1,131 @@
+
+const imgBasePath = "//byui-cit.github.io/cit261/examples/";
+class Hike {
+constructor(name,imgSrc,imgAlt,distance,difficulty,description,directions)
+{
+	this.name = name;
+	this.imgSrc = imgSrc;
+	this.imgAlt = imgAlt;
+	this.distance = distance;
+	this.difficulty = difficulty;
+	this.description = description;
+	this.directions = directions;
+}
+// The HTML needed to render this object on screen
+html()
+{
+  console.log("Rendering");
+  const html = ` <h2>${this.name}</h2>
+        <div class="image"><img src="${this.imgSrc}" alt="${this.imgAlt}"></div>
+        <div>
+                <div>
+                    <h3>Distance</h3>
+                    <p>${this.distance}</p>
+                </div>
+                <div>
+                    <h3>Difficulty</h3>
+                    <p>${this.difficulty}</p>
+                </div>
+        </div>`;
+
+  return html;
+}
+}
+
+Array.prototype.show = function () {
+const hikeListElement = document.getElementById("hikes");
+  hikeListElement.innerHTML = "";
+this.forEach(hike => {
+    const item = document.createElement("li");
+    item.innerHTML = hike.html();
+    hikeListElement.appendChild(item);
+  });
+}
+
+const hikeList = [
+  new Hike(
+"Bechler Falls",
+"falls.jpg",
+"Image of Bechler Falls",
+"3 miles",
+"Easy",
+"Beautiful short hike along the Bechler river to Bechler Falls",
+ "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead."),
+new Hike (
+"Teton Canyon",
+"falls.jpg",
+"Image of Bechler Falls",
+"3 miles",
+"Easy",
+"Beautiful short (or long) hike through Teton Canyon.",
+"Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Staline Raod for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead."
+
+),
+new Hike (
+  "Denanda Falls",
+  "falls.jpg",
+  "Image of Bechler Falls",
+  "7 miles",
+  "Moderate",
+  "Beautiful hike through Bechler meadows river to Denanda Falls",
+  "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead."
+)
+];
+hikeList.show();
+
+/*
+const hikeList = [
+  {
+    name: ,
+    imgSrc: ,
+    imgAlt: ,
+    distance: ,
+    difficulty: ,
+    description:
+      ,
+    directions:
+     
+  },
+  {
+    name: ,
+    imgSrc: 
+    imgAlt: 
+    
+    difficulty: 
+    description: "Beautiful short (or long) hike through Teton Canyon.",
+    directions:
+      "Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Staline Raod for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead."
+  },
+  {
+    name: "Denanda Falls",
+    imgSrc: "falls.jpg",
+    imgAlt: "Image of Bechler Falls",
+    distance: "7 miles",
+    difficulty: "Moderate",
+    description:
+      "Beautiful hike through Bechler meadows river to Denanda Falls",
+    directions:
+      "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead."
+  }
+];
+
+
+
+//on load grab the array and insert it into the page
+/*
+window.addEventListener("load", () => {
+  showHikeList();
+});
+
+function showHikeList() {
+  const hikeListElement = document.getElementById("hikes");
+  hikeListElement.innerHTML = "";
+  renderHikeList(hikeList, hikeListElement);
+}
+
+function renderHikeList(hikes, parent) {
+  hikes.forEach(hike => {
+    parent.appendChild(renderOneHike(hike));
+  });
+}*/
+
