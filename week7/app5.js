4dc8276e1c9f8778059d27b598d3770bd0569eb9
Meg <71366137+meganlynn012@users.noreply.github.com>
2021-06-04 22:19:18 -0600

    added reset to input line

--- week7/app5.js
+++ week7/app5.js
@@ -6,8 +6,32 @@ window.addEventListener('load', () => {
   myHikes.showHikeList();
 });
 
-import { add } from './commenthike.js';
+import { storeComment,getComments } from './commenthike.js';
 document.querySelector(".commentSubmission").addEventListener("submit", (event) => {
   event.preventDefault();
-  add();
+  const commentBody = document.getElementById("commentBody").value;
+  const hikeName = document.getElementById("hikeTitle").value;
+      
+  //removes any text in the input line
+  document.getElementById("commentBody").value = "";
+  storeComment(commentBody,hikeName);
 })
+
+function showCommentsList()
+{
+    const comments = getComments();
+    const commentList = document.getElementById("commentList");
+    commentList.innerHTML = "";
+    for (const i in comments)
+    {
+      const comment = comments[i];
+      let commentListItem = "<li><div>";
+      commentListItem += "<h3>" + comment.hikeName + "</h3>";
+      commentListItem += comment.date + "<br>";
+      commentListItem += comment.content;
+      commentListItem += "</div></li>"
+      commentList.innerHTML += commentListItem;
+    }
+}
+
+showCommentsList();
--- week7/commenthike.js
+++ week7/commenthike.js
@@ -1,27 +1,27 @@
-//let hikeName = document.
-
-let commentList = [];
-
-
-
-function add() {
-    const input = document.getElementById("commentBody").value;
-    console.log(input);
+function getComments()
+{
+    // Return comments as array.
+    const commentsString = window.localStorage.getItem("comments");
+    const comments = JSON.parse(commentsString);
+    return comments;
 }
-    /*if (input !== "") {
-        const commentObject = {
-            name: hikeName,
-            date: new Date(),
-            content: comment,
-            id: Date.now()
-        }
-        commentList.push(commentObject);
-        storage(commentList);
-        document.getElementById("commentBody").value= "";
-     }
-};
 
-function storage(commentList) {
-    console.log(commentList);
-}*/
-export { add };
+function storeComment(commentBody, hikeName) {
+    const comment = {
+        content: commentBody,
+        hikeName: hikeName,
+        date: new Date()
+    };
+    // Add input to local storage.
+    let comments = getComments();
+    if (!comments)
+    {
+        console.log("Initializing comment array.");
+        comments = [];
+    }
+    comments.push(comment);
+    // localStorage only supports strings, so store the array in the form of a JSON string.
+    window.localStorage.setItem("comments",JSON.stringify(comments));
+}
+
+export {storeComment, getComments}
