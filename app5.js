17f15e5796660ec7d78f4a907e60c318c2a5477b
Montana Burr <bur18019@byui.edu>
2021-06-04 20:13:22 -0600

    Shows a list of comments.

--- app5.js
+++ app5.js
@@ -6,10 +6,29 @@ window.addEventListener('load', () => {
   myHikes.showHikeList();
 });
 
-import { storeComment } from './commenthike.js';
+import { storeComment,getComments } from './commenthike.js';
 document.querySelector(".commentSubmission").addEventListener("submit", (event) => {
   event.preventDefault();
   const commentBody = document.getElementById("commentBody").value;
   const hikeName = document.getElementById("hikeName").value;
   storeComment(commentBody,hikeName);
 })
+
+function showCommentsList()
+{
+    const comments = getComments();
+    const commentList = document.getElementById("commentList");
+    commentList.innerHTML = "";
+    for (const i in comments)
+    {
+      const comment = comments[i];
+      let commentListItem = "<li><div>";
+      commentListItem += "<h3>" + comment.hikeName + "</h3>";
+      commentListItem += comment.date + "<br>";
+      commentListItem += comment.content;
+      commentListItem += "</div></li>"
+      commentList.innerHTML += commentListItem;
+    }
+}
+
+showCommentsList();
