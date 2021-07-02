56b4a49f176a7d597523a25b603e9be6cd07b514
Shane Artman <MrShaneArtman@Gmail.com>
2021-07-01 22:58:22 -0400

    Week 11 Work - Login test validation with form

--- week11/client/week11.js
+++ week11/client/week11.js
@@ -1,6 +1,14 @@
+import Auth from './auth.js';
 import makeRequest from './authHelpers.js';
 
-makeRequest('login', 'POST', {
-    password: 'user1',
-    email: 'user1@email.com'
+
+// makeRequest('login', 'POST', {
+//     password: 'user1',
+//     email: 'user1@email.com'
+// });
+
+const auth = new Auth();
+document.querySelector(".submit").addEventListener('click', (data) => {
+    data.preventDefault();
+    auth.login(data);
 });
